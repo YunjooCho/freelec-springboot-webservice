a8c71479b671a801d0a70f1005a37ca631c984ce
conan <conan@naver.com>
2021-05-10 00:44:51 +0900

    gitIgnore

---
+++ src/main/java/com/yjcho/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.yjcho.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application { //프로젝트 메인 클래스
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
